a8e5829a9c56b838b757fc74204dd5a01a80de3e
Zaprovic <jazaro2001@gmail.com>
2025-04-29 09:07:14 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Inspection Application',
+      title: 'Inspection Application change',
       theme: AppTheme.lightTheme,
       initialRoute: '/',
       routes: {
